9d50e0311e168b03ff9e4d8c2a1a27d84b3c290d
Nicholas Delpino <NBDelpino4@gmail.com>
2017-04-17 22:38:53 -0400

    added new files for app make over, started slide view

---
+++ PastVersions/README.md
@@ -0,0 +1,10 @@
+#### Firebase App
+
+#####Second Iteration Update - Day One
+![image](https://cloud.githubusercontent.com/assets/18251657/22537963/f9e89d0e-e8dc-11e6-817f-aac1f5cace02.png)
+
+#####Original Version:
+
+![image](https://cloud.githubusercontent.com/assets/18251657/22537992/3fae5216-e8dd-11e6-9c2c-3423c28f2ac2.png)
+
+
---
+++ PastVersions/assets/javascript/redo.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Nicky on 2/1/17.
+ */
+
+$( document ).ready(function(){
+
+    // SLIDER
+    $('.slider').slider({full_width: true});
+
+    // FLIGHT SECTION
+    $('ul.tabs').tabs();
+
+    // ENABLE TAB STYLING WHEN IN USE
+    $('#menu ul li a').click(function (ev) {
+        $('#menu ul li').removeClass('selected');
+        $(ev.currentTarget).parent('li').addClass('selected');
+    });
+
+    // NAVBAR SLIDE OUT
+    $(".button-collapse").sideNav();
+
+    // DATE PICKER
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+
+    // AUTO COMPLETE
+    $('input.autocomplete').autocomplete({
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'http://placehold.it/250x250'
+        }
+    });
+
+    // BOX DROP DOWN SELECTION
+    $('select').material_select();
+
+});
+
+
